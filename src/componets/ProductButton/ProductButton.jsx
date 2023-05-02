0a2e67393e3a91d65e369ec03dae765b3c2b9677
mucilli.mario <mucilli.mario@gmail.com>
2023-05-02 09:11:45 -0300

    primer carga

---
+++ src/componets/ProductButton/ProductButton.jsx
@@ -0,0 +1,10 @@
+import './ProductButton.css'
+
+const ProductButton = () => {
+    return (
+        <button className="product-button">Agregar al carrito</button>
+
+    )
+}
+
+export default ProductButton
---
+++ src/componets/ProductImage/ProductImage.jsx
@@ -0,0 +1,9 @@
+import "./ProductImage.css"
+
+const ProductImage = () => {
+    return (
+        <img className="product-image" src="https://http2.mlstatic.com/D_NQ_NP_2X_817833-MLA25866381130_082017-F.webp" alt="" />
+    )
+}
+
+export default ProductImage
